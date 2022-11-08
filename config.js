ba00a20bb1a09c406fe3ec7f9c4fb075697a4a8b
VictorSkyLord <94020140+VictorSkyLord@users.noreply.github.com>
2022-11-07 21:18:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAKuIQc8FpABtK0l4kvxpVZUqgkjuLNPE0",
+  authDomain: "itrampo-dea94.firebaseapp.com",
+  databaseURL: "https://itrampo-dea94-default-rtdb.firebaseio.com",
+  projectId: "itrampo-dea94",
+  storageBucket: "itrampo-dea94.appspot.com",
+  messagingSenderId: "265631133809",
+  appId: "1:265631133809:web:b81d700b0781af0c07ebd0"
+};
